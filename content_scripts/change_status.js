a8a3507468be684392920bde06666dbabe424b6c
Drew Rutherford <andrew.rutherford89@gmail.com>
2022-05-19 11:38:02 -0400

    2.0 Release Notes: - Replaced click method for buttons - Removed and added error checking in various places - Cleaned up a lot code

--- content_scripts/change_status.js
+++ content_scripts/change_status.js
@@ -1,8 +1,8 @@
 (function () {
-	
+
 	if (window.hasRun) {
-    return;
-  }
+		return;
+	}
 	window.hasRun = true;
 
 	var backlogInterval;
@@ -10,28 +10,17 @@
 	var refreshInterval;
 	var onlinestatus;
 	var awaystatus;
-	var evt;
 	var str;
-	var backlogstatus;
-	var availableStatus;
-	var dropDown;
-	var clickBacklog;
-	var clickAvailable;
-	var clickdropDown;
-	var omniAction;
-	var refreshButton;
-	var clickRefresh;
-	var omniDropDown;
-	var clickomniDropDown;
+	var StatusDropdownButton;
+	var OmniSuperAction;
 
-  
 	function changeToBacklog() {
 		try {
 			awaystatus = document.getElementsByClassName("awayStatus truncatedText uiOutputText")[0].innerHTML;
 		}
 		catch {
 			awaystatus = "placeholder";
-        }
+		}
 		if (awaystatus.includes("Backlog")) {
 			console.log("Your status is already set to Backlog. Nothing else to do here.");
 		}
@@ -39,31 +28,25 @@
 			console.log("You're currently on a case. Nothing else to do here.");
 		}
 		else {
-			evt = document.createEvent("MouseEvents");
-			evt.initMouseEvent("click", true, true, window,
-				0, 0, 0, 0, 0, false, false, false, false, 0, null);
 			str = document.getElementsByClassName("slds-dropdown__item awayStatus")[0];
 			try {
-				backlogstatus = str.getElementsByTagName("a")[0];
-				clickBacklog = !backlogstatus.dispatchEvent(evt);
+				var backlogstatus = str.getElementsByTagName("a")[0].click();
 				chrome.runtime.sendMessage({
 					command: "backlogNotification"
 				});
 			}
 			catch (error) {
-				console.log("Unable to set status to Backlog. Reason:");
-				console.log(error);
-				console.log("Attempting to fix...");
 				alert("Omni-Channel error detected. Please check console for the detailed error");
+				console.log("Unable to set status to Backlog due to:", error);
+				console.log("Attempting to fix...");
 				try {
-					dropDown = document.getElementsByClassName("slds-button slds-button_icon-container slds-button_icon-x-small")[4];
-					clickdropDown = !dropDown.dispatchEvent(evt);
-					clickdropDown = !dropDown.dispatchEvent(evt);
+					StatusDropdownButton = document.getElementsByClassName("slds-button slds-button_icon-container slds-button_icon-x-small")[4];
+					StatusDropdownButton.click();
+					StatusDropdownButton.click();
 					console.log("Omni-Channel has been fixed. Status will change to Backlog at the next health check.");
 				}
 				catch (error) {
-					console.log("Unable to fix Omni-Channel. Reason:");
-					console.log(error);
+					console.log("Unable to fix Omni-Channel due to:", error);
 					console.log("Please manually set your status to fix the issue");
 				}
 			}
@@ -80,9 +63,9 @@
 		try {
 			awaystatus = document.getElementsByClassName("awayStatus truncatedText uiOutputText")[0].innerHTML;
 		}
-		catch {
+		catch (error) {
 			awaystatus = "placeholder";
-        }
+		}
 		if (onlinestatus.includes("Available")) {
 			console.log("Your status is already set to Available. Nothing else to do here.");
 		}
@@ -90,13 +73,9 @@
 			console.log("You're currently on a case. Nothing else to do here.");
 		}
 		else {
-			evt = document.createEvent("MouseEvents");
-			evt.initMouseEvent("click", true, true, window,
-				0, 0, 0, 0, 0, false, false, false, false, 0, null);
 			str = document.getElementsByClassName("slds-dropdown__item onlineStatus")[0];
 			try {
-				availableStatus = str.getElementsByTagName("a")[0];
-				clickAvailable = !availableStatus.dispatchEvent(evt);
+				var availableStatus = str.getElementsByTagName("a")[0].click();
 				chrome.runtime.sendMessage({
 					command: "availableNotification"
 				});
@@ -107,40 +86,36 @@
 				console.log("Attempting to fix...");
 				alert("Omni-Channel error detected. Please check console for the detailed error");
 				try {
-					dropDown = document.getElementsByClassName("slds-button slds-button_icon-container slds-button_icon-x-small")[4];
-					clickdropDown = !dropDown.dispatchEvent(evt);
-					clickdropDown = !dropDown.dispatchEvent(evt);
+					StatusDropdownButton = document.getElementsByClassName("slds-button slds-button_icon-container slds-button_icon-x-small")[4];
+					StatusDropdownButton.click();
+					StatusDropdownButton.click();
 					console.log("Omni-Channel has been fixed. Status will change to Available at the next health check.");
 				}
 				catch (error) {
-					console.log("Unable to fix Omni-Channel. Reason:");
-					console.log(error);
+					console.log("Unable to fix Omni-Channel due to:", error);
 					console.log("Please manually set your status to fix the issue");
 				}
 			}
 		}
 	}
-		
+
 	function disableHelper() {
 		clearInterval(backlogInterval);
-		backlogInterval = null
+		backlogInterval = null;
 		clearInterval(availableInterval);
-		availableInterval = null
+		availableInterval = null;
 		clearInterval(refreshInterval);
-		refreshInterval = null
+		refreshInterval = null;
 		chrome.runtime.sendMessage({
 			command: "disableNotification"
 		});
 	}
 
+
 	function refreshOmni() {
-		evt = document.createEvent("MouseEvents");
-		evt.initMouseEvent("click", true, true, window,
-			0, 0, 0, 0, 0, false, false, false, false, 0, null);
 		try {
-			omniAction = document.querySelector("[title='Actions for Omni Supervisor']");
-			refreshButton = omniAction.getElementsByClassName("slds-truncate")[0];
-			clickRefresh = !refreshButton.dispatchEvent(evt);
+			OmniSuperAction = document.querySelector("[title='Actions for Omni Supervisor']");
+			var OmniSuperRefreshButton = OmniSuperAction.getElementsByClassName("slds-truncate")[0].click();
 			console.log("Omni Supervisor was successfully refreshed.");
 		}
 		catch (error) {
@@ -148,43 +123,42 @@
 			console.log(error);
 			console.log("Attempting to correct...");
 			try {
-				omniAction = document.querySelector("[title='Actions for Omni Supervisor']");
-				omniDropDown = omniAction.getElementsByClassName("slds-button slds-button_icon-container slds-button_icon-x-small")[0];
-				clickomniDropDown = !omniDropDown.dispatchEvent(evt);
-				clickomniDropDown = !omniDropDown.dispatchEvent(evt);
+				OmniSuperAction = document.querySelector("[title='Actions for Omni Supervisor']");
+				var OmniSuperDropdownButton = OmniSuperAction.getElementsByClassName("slds-button slds-button_icon-container slds-button_icon-x-small")[0];
+				OmniSuperDropdownButton.click();
+				OmniSuperDropdownButton.click();
 				console.log("Error corrected. Omni Supervisor will refresh on the next interval.");
 
 			}
-			catch (error2) {
-				console.log("Could not correct error. Reference the following:");
-				console.log(error2);
+			catch (error) {
+				console.log("Could not correct error due to:", error);
 				cosole.log("Please be sure Omni Supervisor is open within Salesforce.");
 			}
 		}
 	}
-		
+
 	chrome.runtime.onMessage.addListener((message) => {
 		if (message.command === "Backlog") {
 			alert("You have set your Omni-Channel status to Backlog");
 			clearInterval(availableInterval);
-			availableInterval = null
+			availableInterval = null;
 			clearInterval(backlogInterval);
-			backlogInterval = null
+			backlogInterval = null;
 			clearInterval(refreshInterval);
-			refreshInterval = null
-			changeToBacklog()
+			refreshInterval = null;
+			changeToBacklog();
 			backlogInterval = setInterval(changeToBacklog, 15000);
 			refreshInterval = setInterval(refreshOmni, 60000);
 		}
 		else if (message.command === "Available") {
 			alert("You have set your Omni-Channel status to Available");
 			clearInterval(backlogInterval);
-			backlogInterval = null
+			backlogInterval = null;
 			clearInterval(availableInterval);
-			availableInterval = null
+			availableInterval = null;
 			clearInterval(refreshInterval);
-			refreshInterval = null
-			changeToAvailable()
+			refreshInterval = null;
+			changeToAvailable();
 			availableInterval = setInterval(changeToAvailable, 15000);
 			refreshInterval = setInterval(refreshOmni, 60000);
 		}
@@ -193,5 +167,5 @@
 			disableHelper();
 		}
 	});
-						
+
 })();
